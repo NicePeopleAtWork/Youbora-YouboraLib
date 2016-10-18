1d095069410e53f3485bb544d767c729fdc6af25
Joan Treveset <jtreveset@nicepeopleatwork.com>
2016-10-18 10:17:11 +0200

    Files for 5.3.8-beta1

---
+++ tvos/static/YouboraLibTvOS.framework/Headers/YouboraLibTvOS.h
@@ -0,0 +1,33 @@
+//
+//  YouboraLibTvOS.h
+//  YouboraLibTvOS
+//
+//  Created by Joan on 01/07/16.
+//  Copyright © 2016 Nice People At Work. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YouboraLibTvOS.
+FOUNDATION_EXPORT double YouboraLibTvOSVersionNumber;
+
+//! Project version string for YouboraLibTvOS.
+FOUNDATION_EXPORT const unsigned char YouboraLibTvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YouboraLibTvOS/PublicHeader.h>
+
+
+#import <YouboraLibTvOS/YBAdnalyzerGeneric.h>
+#import <YouboraLibTvOS/YBViewManager.h>
+#import <YouboraLibTvOS/YBInfoManager.h>
+#import <YouboraLibTvOS/YBCDN.h>
+#import <YouboraLibTvOS/YBCDNHeader.h>
+#import <YouboraLibTvOS/YBResourceParser.h>
+#import <YouboraLibTvOS/YBCommunication.h>
+#import <YouboraLibTvOS/YBRequest.h>
+#import <YouboraLibTvOS/YBOptions.h>
+#import <YouboraLibTvOS/YBPluginGeneric.h>
+#import <YouboraLibTvOS/YBUtils.h>
+#import <YouboraLibTvOS/YBLog.h>
+#import <YouboraLibTvOS/YBChrono.h>
+#import <YouboraLibTvOS/YBTimer.h>
